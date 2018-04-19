ae950eba21b803eb158594251ee8e8ef10a1fbd5
张书孟 <zhang_shu_meng@163.com>
2018-04-19 16:02:55 +0800

    [commit]:update

--- react-app/src/routes/Test/AppTabs.js
+++ react-app/src/routes/Test/AppTabs.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 
-import { Tabs, WitheSpace } from 'antd-mobile'
-import { StickyContainer, Sticky } from 'react'
+import './Carousel.css'
+
+import Tabs from 'antd-mobile/lib/tabs'
+import WitheSpace from 'antd-mobile/lib/white-space'
+import StickyContainer from 'react-sticky/lib/Container'
+import Sticky from 'react-sticky/lib/Sticky'
 
 
-
-
-
+const tabs = [
+    { title: 'First Tab' },
+    { title: 'Second Tab' },
+    { title: 'Third Tab' },
+];
 
 class AppTabs extends Component {
 
+    render() {
+        return(
+            <div>
 
-
+            </div>
+        )
+    }
 }
 
 export default AppTabs;
